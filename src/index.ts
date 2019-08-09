8555f84497baef3576ee8c88a4132780720fb03e
paulshen <paulshen0@gmail.com>
2019-08-09 11:47:09 -0700

    Add index exports

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import useForceUpdate from "./useForceUpdate";
+import usePersistedMemo from "./usePersistedMemo";
+import usePersistedValue from "./usePersistedValue";
+
+export { useForceUpdate, usePersistedMemo, usePersistedValue };
