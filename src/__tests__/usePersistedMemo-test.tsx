362404bdd4e5088d999e483eb6b2d0585499b672
paulshen <paulshen0@gmail.com>
2019-08-09 17:35:52 -0700

    Test usePersistedMemo

---
+++ src/__tests__/usePersistedMemo-test.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { render, cleanup } from "@testing-library/react";
+import usePersistedMemo from "../usePersistedMemo";
+
+describe("usePersistedMemo", () => {
+  afterEach(cleanup);
+
+  it("works like memo", () => {
+    const factory = jest.fn(deps => deps);
+    const renderMock = jest.fn();
+
+    interface MyComponentProps {
+      deps: Array<number>;
+    }
+    const MyComponent = ({ deps }: MyComponentProps) => {
+      const value = usePersistedMemo(() => factory(deps), deps);
+      renderMock(value);
+      return <div />;
+    };
+
+    const deps = [1, 2];
+    const { rerender } = render(<MyComponent deps={deps} />);
+    expect(factory).toHaveBeenCalledTimes(1);
+    expect(renderMock).toHaveBeenCalledTimes(1);
+    expect(renderMock.mock.calls[renderMock.mock.calls.length - 1][0]).toBe(
+      deps
+    );
+
+    rerender(<MyComponent deps={[1, 2]} />);
+    expect(factory).toHaveBeenCalledTimes(1);
+    expect(renderMock).toHaveBeenCalledTimes(2);
+    expect(renderMock.mock.calls[renderMock.mock.calls.length - 1][0]).toBe(
+      deps
+    );
+
+    const newDeps = [2, 3];
+    rerender(<MyComponent deps={newDeps} />);
+    expect(factory).toHaveBeenCalledTimes(2);
+    expect(renderMock).toHaveBeenCalledTimes(3);
+    expect(renderMock.mock.calls[renderMock.mock.calls.length - 1][0]).toBe(
+      newDeps
+    );
+  });
+});
